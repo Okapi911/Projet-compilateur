ad2293b99ee0e3cd4c11153be85d6af0ecfab6d3
ElieThellier <eliethellier@gmail.com>
2022-10-27 17:08:36 +0200

    ajout de tout, on gerera .gitignore plus tard

--- ouf_func.asm
+++ ouf_func.asm
@@ -3,23 +3,27 @@ section .data
 fmt : db "%d", 10, 0
 argc : dq 0
 argv : dq 0
+y : dq 0
 x : dq 0
 
 section .text
 
 
-        f:
-            push rbp
-            mov rbp, rsp
-            mov rsp, [rbp-8]
-            
-            mov rax, [y]
+f:
+    push rbp
+    mov rbp, rsp
+    mov rdi, [rbx+8]
+    xor rax, rax
+    call atoi
+    mov [y], rax
+    
+    mov rax, [y]
 
-            mov rsp, rax
-            mov rsp, rbp
-            pop rbp
-            ret
-        
+    mov rsp, rax
+    mov rsp, rbp
+    pop rbp
+    ret
+    
 
 global main
 main :
@@ -27,16 +31,16 @@ main :
     mov [argc], rdi
     mov [argv], rsi
     
-        mov rbx, [argv]
-        mov rdi, [rbx + 8]
-        xor rax, rax
-        call atoi
-        mov [x], rax
+    mov rbx, [argv]
+    mov rdi, [rbx + 8]
+    xor rax, rax
+    call atoi
+    mov [x], rax
         
     
     
-        push x
-        call f
+    push x
+    call f
         
     mov rdi, fmt
     mov rsi, rax
